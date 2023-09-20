592816be5d090c28c35baf09aa5c64c229fcfbd3
rahilpasha <rahilpasha006@gmail.com>
2023-09-20 09:45:20 -0700

    create API method for getting nutrition data

--- CalorieBalance/App.js
+++ CalorieBalance/App.js
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+function getFoodNutrition(food) {
+  food_url = "https://trackapi.nutritionix.com/v2/natural/nutrients";
+
+  return fetch(food_url, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'x-app-id': '3d415188',
+      'x-app-key': '202343b02beb731a678bdef385ee803d',
+      'x-remote-user-id': '0'
+    },
+    body: JSON.stringify({
+      'query': food
+    })
+  })
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch((error) => {
+    console.error(error);
+  });
+
+}
+
 export default function App() {
+  getFoodNutrition('1 cup chicken noodle soup')
   return (
     <View style={styles.container}>
-      <Text>waga</Text>
+      <Text>1 cup chicken noodle soup</Text>
       <StatusBar style="auto" />
     </View>
   );
