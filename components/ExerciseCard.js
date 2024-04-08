c71cc2ed2add92debfb18bfbb45bff449906ed20
rahilpasha <rahilpasha006@gmail.com>
2024-04-08 16:54:18 -0700

    created components folder

---
+++ components/ExerciseCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View, Button, Text} from 'react-native';
+
+const ExercsieCard = (name, calories) => {
+	return (
+		// the code for the card goes in here
+
+		<View style={exerciseStyles.card}>
+
+			
+
+		</View>
+
+
+	);
+};
+
+const exerciseStyles = StyleSheet.create({
+	card: {
+		backgroundColor: '#111818'
+	}
+});
