95c0eaccabb43ea455af95411bedbc41a5b221d1
Riddy06 <143293349+Riddy06@users.noreply.github.com>
2024-04-08 21:31:33 -0500

    Update ExerciseCard.js
    I changed the color to pink and centered it

--- components/ExerciseCard.js
+++ components/ExerciseCard.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import {View, Button, Text} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-const ExercsieCard = (name, calories) => {
-	return (
-		// the code for the card goes in here
-
-		<View style={exerciseStyles.card}>
-
-			
-
-		</View>
-
-
-	);
+const ExerciseCard = ({ name, calories }) => {
+  return (
+    <View style={exerciseStyles.container}>
+      <View style={exerciseStyles.card}>
+        <Text style={exerciseStyles.name}>{name}</Text>
+        <Text style={exerciseStyles.calories}>{calories} calories</Text>
+      </View>
+    </View>
+  );
 };
 
 const exerciseStyles = StyleSheet.create({
-	card: {
-		backgroundColor: '#111818'
-	}
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card: {
+    backgroundColor: 'rgb(235, 169, 169)', // Pink color
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  calories: {
+    fontSize: 14,
+    color: '#fff',
+  },
 });
+
+export default ExerciseCard;
